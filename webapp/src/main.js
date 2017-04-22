50536c4eedcf059ab7edb77ca9bd0ef42e654242
wangxin <wangxin@trustedtech.com.cn>
2017-04-22 19:32:59 +0800

    add font-awesome and vue-router

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
+import 'font-awesome/css/font-awesome.css'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
-Vue.use(ElementUI)
+Vue.use(ElementUI);
+Vue.use(VueRouter);
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
