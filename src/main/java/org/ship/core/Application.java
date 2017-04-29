b899b0dd1a8f1cafaa831be6d415622779727f77
wangxin <wangxin@trustedtech.com.cn>
2017-04-29 18:45:14 +0800

    add mysql and mybatis

--- src/main/java/org/ship/core/Application.java
+++ src/main/java/org/ship/core/Application.java
@@ -1,9 +1,11 @@
 package org.ship.core;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.ship.core.dao")
 public class Application {
 
 	public static void main(String[] args) {
